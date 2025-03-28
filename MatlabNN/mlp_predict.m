e4b5ca56f32b1ffecd742653db46ac0da2d518b8
hunger2K <45189000+hunger2K@users.noreply.github.com>
2025-03-28 08:10:35 -0700

    small edits

--- MatlabNN/mlp_predict.m
+++ MatlabNN/mlp_predict.m
@@ -1,4 +1,8 @@
 function y = mlp_predict(x)
-    load('test.mat', 'trainedNetwork');
-    y = trainedNetwork(x);
+    persistent net
+    if isempty(net)
+        data = load('MLP1.0.0.mat');
+        net = data.net;  % or whatever variable name you saved it as
+    end
+    y = net(x);  % or y = predict(net, x) depending on network type
 end
