33c21225f8531f5492b1a61fa63abcdda9ea715b
shawnzeeee <shawnxie0125@gmail.com>
2025-03-28 15:43:01 -0700

    added gestures

--- HandGestureDataCollection/g.Nautilus/gNautilusDemo.cpp
+++ HandGestureDataCollection/g.Nautilus/gNautilusDemo.cpp
@@ -34,9 +34,9 @@
 
 // Definition of network specific stuff.
 //-------------------------------------------------------------------------------------
-#define HOST_IP "142.58.221.37" // Default address is the loopback address, else the ip of the computer running GDS.
+#define HOST_IP "207.23.181.48" // Default address is the loopback address, else the ip of the computer running GDS.
 #define HOST_PORT 50223     // The default port of GDS is 50223.
-#define LOCAL_IP "207.23.209.165"// Default address is the loppback address, else the ip of the client machine.
+#define LOCAL_IP "207.23.206.235"// Default address is the loppback address, else the ip of the client machine.
 #define LOCAL_PORT 50224    // Any free port on the local machine.
 
 // Function declarations.
