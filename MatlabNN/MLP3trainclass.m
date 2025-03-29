94e9bbc901e5b5278d59ca2e01c1cb9423be72e0
hunger2K <45189000+hunger2K@users.noreply.github.com>
2025-03-29 13:10:19 -0700

    added python feature extract

--- MatlabNN/MLP3trainclass.m
+++ MatlabNN/MLP3trainclass.m
@@ -1,17 +1,15 @@
 %Define the Ytrain matrix : define which class this is
 
-%Class0    % Nothing
+%Class0     % Nothing
 %Class1     % Thumb open
 %Class2     % Thumb close
 %Class3     % Index open
 %Class4     % Index close
 %Class5     % Middle open
 %Class6     % Middle close
-%Class7     % Ring open
-%Class8     % Ring close
-%Class9     % Pinky open
-%Class10     % Pinky close
-%Class11    % Full hand open
-%Class12    % Full hand close
+%Class7     % Ring and pinky open
+%Class8     % Ring and pinky close
+%Class9     % Full hand open
+%Class10    % Full hand close
 Class = 12;
 YClasses = categorical(Class, 0:12);
