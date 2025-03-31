26252f79261aa99f61c60ec74e428524e5d4fdb1
hunger2K <45189000+hunger2K@users.noreply.github.com>
2025-03-31 16:48:30 -0700

    New trials Nick BP2, and added matlabplotting3min

--- MatlabNN/bintoPlot3min.m
+++ MatlabNN/bintoPlot3min.m
@@ -1,4 +1,5 @@
-fid = fopen('EEG_Recordings/Daniel/3minIndexOpenClose_BP2/trial1.bin', 'r');
+%fid = fopen('EEG_Recordings/Daniel/3minIndexOpenClose_BP2/trial1.bin', 'r');
+fid = fopen('EEG_Recordings/Nick/3minBaseline_BP2/trial2.bin', 'r');
 
 data = fread(fid, 'float32'); % Adjust format if needed
 fclose(fid);
@@ -39,28 +40,28 @@ for i = 1:4
     %Class9     % Full hand open
     %Class10    % Full hand close
     
-    %openThumb = find(segment_class == 1);
-    %closeThumb = find(segment_class == 2);
+    openThumb = find(segment_class == 1);
+    closeThumb = find(segment_class == 2);
     openIndex = find(segment_class == 3);
     closeIndex = find(segment_class == 4);
-    %openMiddle = find(segment_class == 5);
-    %closeMiddle = find(segment_class == 6);
-    %openRingPinky = find(segment_class == 7);
-    %closeRingPinky = find(segment_class == 8);
-    %openHand = find(segment_class == 9);
-    %closeHand = find(segment_class == 10);
+    openMiddle = find(segment_class == 5);
+    closeMiddle = find(segment_class == 6);
+    openRingPinky = find(segment_class == 7);
+    closeRingPinky = find(segment_class == 8);
+    openHand = find(segment_class == 9);
+    closeHand = find(segment_class == 10);
     
     % Overlay non-zero class marker
-    %scatter(timestamps(sTime + openThumb), channel_data(sTime + openThumb, i), 20, 'g', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + closeThumb), channel_data(sTime + closeThumb, i), 20, 'r', 'filled', 'DisplayName', 'Non-zero Class');
-    scatter(timestamps(sTime + openIndex), channel_data(sTime + openIndex, i), 20, 'g', 'filled', 'DisplayName', 'Non-zero Class');
-    scatter(timestamps(sTime + closeIndex), channel_data(sTime + closeIndex, i), 20, 'r', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + openMiddle), channel_data(sTime + openMiddle, i), 20, 'g', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + closeMiddle), channel_data(sTime + closeMiddle, i), 20, 'r', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + openRingPinky), channel_data(sTime + openRingPinky, i), 20, 'g', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + closeRingPinky), channel_data(sTime + closeRingPinky, i), 20, 'r', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + openHand), channel_data(sTime + openHand, i), 20, 'g', 'filled', 'DisplayName', 'Non-zero Class');
-    %scatter(timestamps(sTime + closeHand), channel_data(sTime + closeHand, i), 20, 'r', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + openThumb), channel_data(sTime + openThumb, i), 20, 'yellow', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + closeThumb), channel_data(sTime + closeThumb, i), 20, 'red', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + openIndex), channel_data(sTime + openIndex, i), 20, 'blue', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + closeIndex), channel_data(sTime + closeIndex, i), 20, 'red', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + openMiddle), channel_data(sTime + openMiddle, i), 20, 'blue', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + closeMiddle), channel_data(sTime + closeMiddle, i), 20, 'red', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + openRingPinky), channel_data(sTime + openRingPinky, i), 20, 'cyan', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + closeRingPinky), channel_data(sTime + closeRingPinky, i), 20, 'red', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + openHand), channel_data(sTime + openHand, i), 20, 'green', 'filled', 'DisplayName', 'Non-zero Class');
+    scatter(timestamps(sTime + closeHand), channel_data(sTime + closeHand, i), 20, 'red', 'filled', 'DisplayName', 'Non-zero Class');
 
     title(['Channel ', num2str(i)]);
     xlabel('Timestamp');
