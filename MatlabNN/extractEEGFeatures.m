1025369a95cb482b6a2442663d2bc714d0425234
hunger2K <45189000+hunger2K@users.noreply.github.com>
2025-04-01 13:21:29 -0700

    pushed automation training 2 and python eegfeatures extract

--- MatlabNN/extractEEGFeatures.m
+++ MatlabNN/extractEEGFeatures.m
@@ -6,11 +6,22 @@ function features = extractEEGFeatures(eeg_signal, fs)
     % Output:
     % features - Feature vector containing extracted values
     
+    % Power spectral density using Welch's method
+    [pxx, f] = pwelch(eeg_signal, hamming(256), 128, 512, fs);
+
+    % Alpha power (8–13 Hz)
+    alphaIdx = f >= 8 & f <= 13;
+    alpha_power = bandpower(pxx, f, [8 13],'psd');
+
+    % Beta power (13–30 Hz)
+    betaIdx = f >= 13 & f <= 30;
+    beta_power = bandpower(pxx, f, [13 30],'psd');
+
     %   Power in Alpha (8-13 Hz) and Beta (13-30 Hz) Bands
     %[pxx, f] = pwelch(eeg_signal, [], [], [], fs);  % Power Spectral Density
     % Compute Alpha and Beta Band Power Directly from Time-Domain Signal
-    alpha_power = bandpower(eeg_signal, fs, [8 13]);  % Alpha (8-13 Hz)
-    beta_power = bandpower(eeg_signal, fs, [13 30]);  % Beta (13-30 Hz)
+    %alpha_power = bandpower(eeg_signal, fs, [8 13]);  % Alpha (8-13 Hz)
+    %beta_power = bandpower(eeg_signal, fs, [13 30]);  % Beta (13-30 Hz)
 
     %Root Mean Square (RMS)
     rms_value = rms(eeg_signal);
